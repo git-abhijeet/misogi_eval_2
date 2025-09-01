99e2774649d1f14ca0c32ec91fc7e9648b8e7d51
abhijeet <akc972527@gmail.com>
2025-09-01 13:35:39 +0000

    added pagination

--- controllers/transaction_controller.py
+++ controllers/transaction_controller.py
@@ -4,15 +4,24 @@ from fastapi import HTTPException
 from datetime import datetime
 
 
-async def user_transaction(user_id: str):
-    # user = user_collection.find_one({"user_id": ObjectId(user_id)})
-    # if not user:
-    #     raise ValueError("User not found")
+async def user_transaction(user_id: str, page: int = 1, limit: int = 10):
+    user = user_collection.find_one({"_id": ObjectId(user_id)})
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
 
-    transactions = list(transaction_collection.find({"user_id": user_id}))
+    skip = (page - 1) * limit
+    transactions = list(transaction_collection.find({"user_id": user_id}).skip(skip).limit(limit))
     for transaction in transactions:
         transaction["_id"] = str(transaction["_id"])
-    return transactions
+    
+    total_transactions = transaction_collection.count_documents({"user_id": user_id})
+
+    return {
+        "transactions": transactions,
+        "total": total_transactions,
+        "page": page,
+        "limit": limit
+    }
 
 
 async def transaction_detail(transaction_id: str):
--- handlers/transaction_handler.py
+++ handlers/transaction_handler.py
@@ -1,11 +1,11 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Query
 from controllers.transaction_controller import user_transaction, transaction_detail
 
 
 
-async def get_user_transactions_handler(user_id: str):
+async def get_user_transactions_handler(user_id: str, page: int = Query(1, ge=1), limit: int = Query(10, ge=1, le=100)):
     try:
-        transaction = await user_transaction(user_id)
+        transaction = await user_transaction(user_id, page, limit)
     except Exception as e: 
         raise HTTPException(status_code=500, detail=str(e))
     return transaction
